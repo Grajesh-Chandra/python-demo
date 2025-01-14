294b08404e8dc784d64083dd32f973b8948ac322
Paramesh Kamarthi <kamarthiparamesh@gmail.com>
2025-01-14 13:03:34 +0530

    feat: added signature vc to PDF

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template, jsonify, request, send_file
+from flask import Flask, Response, render_template, jsonify, request, send_file
+
+from cryptography.hazmat.primitives.hashes import Hash, SHA256
 from . import app
 import affinidi_tdk_auth_provider
 import affinidi_tdk_credential_issuance_client
 import logging
 from pypdf import PdfWriter, PdfReader
 from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.utils import ImageReader
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
+
 import json
 from io import BytesIO
 import base64
 import affinidi_tdk_credential_verification_client
 import requests
 import os
+import hashlib
+import qrcode
 
 # api_gateway_url = app.config["api_gateway_url"]
 # token_endpoint = app.config["token_endpoint"]
@@ -267,7 +276,8 @@ def issue_credentials():
             )
 
             projectId = "97dde52f-e99c-420e-885a-3cc3032e73d9"
-            request_json = {"data": credentials_request, "claimMode": "TX_CODE"}
+            request_json = {"data": credentials_request,
+                            "claimMode": "TX_CODE"}
             print("request_json", request_json)
 
             start_issuance_input = (
@@ -293,6 +303,59 @@ def issue_credentials():
         return jsonify({"error": "An error occurred"}), 500
 
 
+def get_file_content_buffer(credential):
+    json_buffer = BytesIO()
+    json_buffer.write(json.dumps(
+        credential).encode("utf-8"))
+    json_buffer.seek(0)
+    return json_buffer
+
+
+def generate_qr_code(url):
+    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    qr_image = qr.make_image(fill_color="black", back_color="white")
+    qr_buffer = BytesIO()
+    qr_image.save(qr_buffer, format="PNG")
+    qr_buffer.seek(0)
+    return qr_buffer
+
+def create_table(canvas,):
+    data = [
+        ["Candidate/Employee Full Name", "Paramesh K", "Order ID", "241004.1021"],
+        ["Company Name", "Affinidi", "Branch Name", ""],
+        ["Date of Report", "14-01-2025", "Cost Centre", "-"],
+        ["Package Code/Level (if any)", "", "Case Reference No.", "AV041024MTIxNDU0"],
+        ["Result", "Completed", "Result", "GREEN"]
+    ]
+    table = Table(data, colWidths=[150, 100, 150, 150])
+
+    # (attribute, (start_column, start_row), (end_column, end_row), value)
+    style = TableStyle([
+        ('BACKGROUND', (0, 0), (0, -1), colors.yellow), # First column
+        ('BACKGROUND', (2, 0), (2, -1), colors.yellow), # 3rd column
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),   # Text color for the header row
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),    # Add grid lines
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),          # Center-align all text
+    ])
+    table.setStyle(style)
+
+    # Calculate table position (x, y coordinates on the page)
+    table_width, table_height = table.wrap(0, 0)  # Get table dimensions
+    page_width, page_height = A4  
+
+    # Set margins (e.g., 50 points on left and right)
+    margin = 30
+
+    x = (page_width - table_width + margin) / 2                 # Center the table horizontally
+    y = page_height - 200                        # Position the table below the header text
+
+    # Draw the table on the canvas
+    table.drawOn(canvas, x, y)
+
+
 @app.route("/api/generate-report", methods=["POST"])
 def generate_report():
     try:
@@ -302,17 +365,38 @@ def generate_report():
 
         # Generate initial PDF content using reportlab
         pdf_buffer = BytesIO()
-        c = canvas.Canvas(pdf_buffer)
+        c = canvas.Canvas(pdf_buffer, pagesize=A4)
+
+        # Add content to the first page
         c.drawString(100, 750, f"Report Type: {report_type}")
+
+        # Create table
+        create_table(c)
+
+        c.showPage()  # End the first page
+
+        url = "https://verification.avvanz.com"
+
+        c.drawString(100, 750, f"Click here ({url}) for Verification")
+        # Add clickable URL link
+        c.linkURL(url, (100, 740, 300, 760), relative=0)
+
+        # Generate the QR code
+        qr_buffer = generate_qr_code(url)
+        qr_reader = ImageReader(qr_buffer)
+        c.drawImage(qr_reader, 100, 600, width=100, height=100)
+
+        # Finalize the PDF
         c.save()
-        pdf_buffer.seek(0)
 
         # Create a PdfReader to read the reportlab output
+        pdf_buffer.seek(0)
         pdf_reader = PdfReader(pdf_buffer)
-        page = pdf_reader.pages[0]  # Get the page
 
+        # Write the new PDF
         pdf_writer = PdfWriter()
-        pdf_writer.add_page(page)  # Add the PageObject
+        for page in pdf_reader.pages:
+            pdf_writer.add_page(page)
 
         # Create JSON data
         AddressVerification_credentials = {
@@ -363,12 +447,12 @@ def generate_report():
                 "jws": "eyJhbGciOiJFUzI1NksiLCJiNjQiOmZhbHNlLCJjcml0IjpbImI2NCJdfQ..Jiq6yE6BTq9wXI2QT8-174_BAA-W2fdEF1d5DaUXEC1gMr62zSw2pGL_fl_eIBUPwKsOgc6TA0E1_rMmD8BOKA",
             },
         }
-        json_buffer = BytesIO()
-        json_buffer.write(json.dumps(AddressVerification_credentials).encode("utf-8"))
-        json_buffer.seek(0)
+
+        json_buffer = get_file_content_buffer(AddressVerification_credentials)
 
         # Attach JSON to PDF
-        pdf_writer.add_attachment("AddressVerification.json", json_buffer.getbuffer())
+        pdf_writer.add_attachment(
+            "AddressVerification.json", json_buffer.getbuffer())
 
         # Create JSON data
         PersonalInformationVerification_credentials = {
@@ -419,11 +503,9 @@ def generate_report():
                 "jws": "eyJhbGciOiJFUzI1NksiLCJiNjQiOmZhbHNlLCJjcml0IjpbImI2NCJdfQ..Jiq6yE6BTq9wXI2QT8-174_BAA-W2fdEF1d5DaUXEC1gMr62zSw2pGL_fl_eIBUPwKsOgc6TA0E1_rMmD8BOKA",
             },
         }
-        json_buffer = BytesIO()
-        json_buffer.write(
-            json.dumps(PersonalInformationVerification_credentials).encode("utf-8")
-        )
-        json_buffer.seek(0)
+
+        json_buffer = get_file_content_buffer(
+            PersonalInformationVerification_credentials)
 
         # Attach JSON to PDF
         pdf_writer.add_attachment(
@@ -493,12 +575,13 @@ def generate_report():
                 },
             },
         )
-        json_buffer = BytesIO()
-        json_buffer.write(json.dumps(EducationVerification_credentials).encode("utf-8"))
-        json_buffer.seek(0)
+
+        json_buffer = get_file_content_buffer(
+            EducationVerification_credentials)
 
         # Attach JSON to PDF
-        pdf_writer.add_attachment("EducationVerification.json", json_buffer.getbuffer())
+        pdf_writer.add_attachment(
+            "EducationVerification.json", json_buffer.getbuffer())
 
         # Create JSON data
         EmploymentVerification_credentials = {
@@ -566,15 +649,51 @@ def generate_report():
                 "jws": "eyJhbGciOiJFUzI1NksiLCJiNjQiOmZhbHNlLCJjcml0IjpbImI2NCJdfQ..wsOTPlB_E21Av4JORHBBF-RL5XK6CSvQR9KNvFi8yDBzfRY8JgFsSLRmd9kiiHkk6rYsJeHhWBmL-iY-LG5nlQ",
             },
         }
-        json_buffer = BytesIO()
-        json_buffer.write(
-            json.dumps(EmploymentVerification_credentials).encode("utf-8")
-        )
-        json_buffer.seek(0)
+        json_buffer = get_file_content_buffer(
+            EmploymentVerification_credentials)
 
         # Attach JSON to PDF
         pdf_writer.add_attachment(
             "EmploymentVerification.json", json_buffer.getbuffer()
+        )
+
+        # Create Signature
+        pdf_hash_excluding_attachments = hash_pdf_content_excluding_attachments(
+            pdf_reader)
+        print(pdf_hash_excluding_attachments)
+
+        pdf_signature = {
+            "credentialSchema": {
+                "type": "TEmploymentVerificationV1R1",
+                "id": "https://schema.affinidi.io/TEmploymentVerificationV1R1.json",
+            },
+            "credentialSubject": {
+                "hash_without_attachments": pdf_hash_excluding_attachments
+            },
+            "issuanceDate": "2024-12-02T16:54:12.767Z",
+            "holder": {
+                "id": "did:key:zQ3shs2AM1EMpfDthXaZJmBnjvGbuzMPB94uFAeUkvGe5sM15"
+            },
+            "id": "claimId:8076e3f05e734b4a",
+            "type": ["VerifiableCredential", "TEmploymentVerificationV1R1"],
+            "@context": [
+                "https://www.w3.org/2018/credentials/v1",
+                "https://schema.affinidi.io/TEmploymentVerificationV1R1.jsonld",
+            ],
+            "issuer": "did:key:zQ3shNyLdzYDbwZibk5hinAm5ChzxMinywBJz98fbFcQbn6Xf",
+            "proof": {
+                "type": "EcdsaSecp256k1Signature2019",
+                "created": "2024-12-02T16:54:21Z",
+                "verificationMethod": "did:key:zQ3shNyLdzYDbwZibk5hinAm5ChzxMinywBJz98fbFcQbn6Xf#zQ3shNyLdzYDbwZibk5hinAm5ChzxMinywBJz98fbFcQbn6Xf",
+                "proofPurpose": "assertionMethod",
+                "jws": "eyJhbGciOiJFUzI1NksiLCJiNjQiOmZhbHNlLCJjcml0IjpbImI2NCJdfQ..wsOTPlB_E21Av4JORHBBF-RL5XK6CSvQR9KNvFi8yDBzfRY8JgFsSLRmd9kiiHkk6rYsJeHhWBmL-iY-LG5nlQ",
+            },
+        }
+
+        json_buffer = get_file_content_buffer(pdf_signature)
+        # Attach Signature JSON to PDF
+        pdf_writer.add_attachment(
+            "PDFSignature.json", json_buffer.getbuffer()
         )
 
         # Write the final PDF to a buffer
@@ -595,6 +714,7 @@ def generate_report():
 
 @app.route("/api/verify_pdf", methods=["POST"])
 def verify_pdf():
+
     try:
         if "report_pdf" not in request.files:
             return jsonify({"error": "No PDF file uploaded"}), 400
@@ -604,13 +724,26 @@ def verify_pdf():
 
         pdf_reader = PdfReader(pdf_buffer)
 
+        # Hash the PDF content
+        pdf_hash_excluding_attachments = hash_pdf_content_excluding_attachments(
+            pdf_reader)
+
         attachments = {}
+
+        attachments["PDF Full Hash"] = hash_pdf_content(pdf_buffer)
+
+        attachments["PDF Hash(Without attachments)"] = pdf_hash_excluding_attachments
+
         for filename, data in pdf_reader.attachments.items():
             if isinstance(data, list):  # Check if data is a list
 
-                data = b"".join(data)  # join bytes in list to single bytes object
+                # join bytes in list to single bytes object
+                data = b"".join(data)
 
             try:
+                if (filename != 'PDFSignature.json'):
+                    continue
+
                 # Attempt to decode as JSON
                 json_data = json.loads(data.decode("utf-8"))
                 attachments[filename] = json_data
@@ -666,7 +799,8 @@ def verification(request):
     #     api_instance = affinidi_tdk_credential_verification_client.DefaultApi(api_client)
 
     url = api_gateway_url + f"/ver/v1/verifier/verify-vcs"
-    headers = {"Authorization": f"Bearer {pst()}", "Content-Type": "application/json"}
+    headers = {"Authorization": f"Bearer {
+        pst()}", "Content-Type": "application/json"}
 
     body = {
         "verifiableCredentials": [
@@ -746,3 +880,36 @@ def verification(request):
     # api_response = api_instance.verify_credentials(verify_credentials_input=verify_credentials_input)
     # print('api_response', api_response)
     return api_response
+
+
+def hash_pdf_content(pdf_buffer):
+    # Get the PDF content
+    pdf_buffer.seek(0)
+    pdf_content = pdf_buffer.getvalue()
+
+    # Hash the content
+    pdf_hash = hashlib.sha256(pdf_content).hexdigest()
+    return pdf_hash
+
+
+def hash_pdf_content_excluding_attachments(pdf_reader):
+    try:
+
+        # Create a new PdfWriter to store only the pages
+        pdf_writer = PdfWriter()
+
+        # Add pages from the original PDF, excluding attachments
+        for page in pdf_reader.pages:
+            pdf_writer.add_page(page)
+
+        # Write the new PDF (without attachments) to a temporary buffer
+        temp_buffer = BytesIO()
+        pdf_writer.write(temp_buffer)
+        temp_buffer.seek(0)
+
+        # Hash the content of the new PDF
+        pdf_hash = hashlib.sha256(temp_buffer.getvalue()).hexdigest()
+
+        return pdf_hash
+    except Exception as e:
+        raise RuntimeError(f"Error processing PDF: {e}")
